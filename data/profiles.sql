0bd305dbf8445a4585e6dbeed07e50d765b03b81
romanrurenko <rroman7@yandex.ru>
2020-07-03 12:37:21 +0700

    Added generated files sql

---
+++ data/profiles.sql
@@ -0,0 +1,22 @@
+INSERT INTO `profile` 
+(`address`,`birthday`,`about`,`phone`,`skype`,`city_id`,`user_id`,`messenger`,`avatar`,`rate`,`role`)
+ VALUES ('38737 Moose Avenue','1989-11-11','In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.',64574473047,'high-level',2,19,'telegram','no-avatar.jpg',1,'executor'),
+('738 Hagan Lane','1989-03-05','Pellentesque ultrices mattis odio.',75531015353,'mobile',2,10,'telegram','no-avatar.jpg',1,'executor'),
+('758 Old Shore Parkway','1989-12-30','Morbi a ipsum. Integer a nibh. In quis justo.',16371407508,'Re-engineered',9,13,'telegram','no-avatar.jpg',2,'customer'),
+('11 Dovetail Junction','0629-03-03','Suspendisse potenti.',21468788926,'Grass-roots',10,5,'telegram','no-avatar.jpg',1,'customer'),
+('050 Bowman Alley','1989-04-08','Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.',62931646367,'fault-tolerant',1,18,'telegram','no-avatar.jpg',2,'customer'),
+('5 Iowa Avenue','1989-04-18','Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',63271348718,'Team-oriented',6,14,'telegram','no-avatar.jpg',2,'customer'),
+('87119 Northland Hill','1989-03-20','Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.',41056175169,'portal',10,4,'telegram','no-avatar.jpg',1,'executor'),
+('6823 Lillian Point','1989-12-13','Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.',72882384431,'intermediate',1,20,'telegram','no-avatar.jpg',3,'customer'),
+('43 Marquette Plaza','1989-01-14','Morbi ut odio.',69043821394,'local area network',1,17,'telegram','no-avatar.jpg',1,'customer'),
+('5303 Village Green Hill','1989-02-03','Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.',28396220507,'upward-trending',9,4,'telegram','no-avatar.jpg',1,'executor'),
+('67399 Reindahl Place','1989-05-23','Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.',83344513307,'grid-enabled',9,5,'telegram','no-avatar.jpg',3,'customer'),
+('45 Twin Pines Hill','1989-07-06','Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc.',64890419671,'background',3,15,'telegram','no-avatar.jpg',3,'customer'),
+('46 Sheridan Place','1903-04-16','Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.',23005580487,'challenge',5,13,'telegram','no-avatar.jpg',1,'customer'),
+('73 Kedzie Terrace','1989-11-07','Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',27052074771,'coherent',2,10,'telegram','no-avatar.jpg',2,'executor'),
+('85509 Ludington Drive','1989-02-13','Cras pellentesque volutpat dui.',14800371520,'neutral',8,9,'telegram','no-avatar.jpg',3,'customer'),
+('67 Northwestern Center','1989-07-07','Aliquam erat volutpat. In congue.',75569924500,'Programmable',5,2,'telegram','no-avatar.jpg',2,'customer'),
+('725 Eagle Crest Hill','1989-09-29','Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc.',37349256497,'encompassing',9,16,'telegram','no-avatar.jpg',2,'customer'),
+('507 Graceland Junction','1989-03-19','Suspendisse potenti.',12403580562,'knowledge base',10,18,'telegram','no-avatar.jpg',2,'customer'),
+('92 Gina Park','1989-09-29','Phasellus sit amet erat.',40139478003,'dynamic',1,13,'telegram','no-avatar.jpg',3,'executor'),
+('8 Ridgeview Trail','1989-12-21','Cras pellentesque volutpat dui.',76657531985,'solution',7,5,'telegram','no-avatar.jpg',1,'executor')
---
+++ data/status.sql
@@ -0,0 +1,7 @@
+INSERT INTO `status` 
+(`name`)
+ VALUES ('New'),
+('Cancel'),
+('In_work'),
+('Complete'),
+('Failed')
