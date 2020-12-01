f19cfa473a698d03d12600f7cc285db377855fe1
romanrurenko <rroman7@yandex.ru>
2020-12-01 12:48:41 +0700

    Update task view

--- frontend/views/tasks/view.php
+++ frontend/views/tasks/view.php
@@ -28,6 +28,7 @@ use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 
 $this->registerJSFile('/js/main.js');
+$this->registerJSFile('/js/vue.js');
 $this->registerJSFile('/js/messenger.js');
 $currentUserId = Yii::$app->user->getId();
 ?>
@@ -226,31 +227,9 @@ $currentUserId = Yii::$app->user->getId();
                         || ((int)$modelTask['executor_id']) === $currentUserId)
                     && ($modelTask['executor_id'] !== null)
                 ): ?>
-                    <div class="connect-desk__chat">
-                        <h3>Переписка</h3>
-                        <div class="chat__overflow">
-                            <div class="chat__message chat__message--out">
-                                <p class="chat__message-time">10.05.2019, 14:56</p>
-                                <p class="chat__message-text">Привет. Во сколько сможешь
-                                    приступить к работе?</p>
-                            </div>
-                            <div class="chat__message chat__message--in">
-                                <p class="chat__message-time">10.05.2019, 14:57</p>
-                                <p class="chat__message-text">На задание
-                                    выделены всего сутки, так что через час</p>
-                            </div>
-                            <div class="chat__message chat__message--out">
-                                <p class="chat__message-time">10.05.2019, 14:57</p>
-                                <p class="chat__message-text">Хорошо. Думаю, мы справимся</p>
-                            </div>
-                        </div>
-                        <p class="chat__your-message">Ваше сообщение</p>
-                        <form class="chat__form">
-                        <textarea class="input textarea textarea-chat" rows="2" name="message-text"
-                                  placeholder="Текст сообщения"></textarea>
-                            <button class="button chat__button" type="submit">Отправить</button>
-                        </form>
-                    </div>
+                <div id="chat-container">
+
+                    <chat class="connect-desk__chat" task="<?=$modelTask['id']?>"></chat>
                 <?php
                 endif; ?>
             </section>
--- src/Constant/Page.php
+++ src/Constant/Page.php
@@ -7,12 +7,12 @@ class Page
     const ROUTE_TO_PAGE_NAME = [
         'landing/index' => 'Промо',
         'tasks/index' => 'Задания',
+        'tasks/view' => 'Задание',
         'users/index' => 'Исполнители',
         'users/view' => 'Исполнитель',
         'task/create' => 'Создать задание',
         'profile/index' => 'Мой профиль',
         'my-list/index' => 'Мои задания',
-        'tasks/view' => 'Исполнитель',
         'signup/index' => 'Регистрация',
     ];
 }
