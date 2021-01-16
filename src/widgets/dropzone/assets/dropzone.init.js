2927b26a75ad733e15d76db573ec1fa9e2d2e9a3
romanrurenko <rroman7@yandex.ru>
2021-01-16 15:21:13 +0700

    Adds support for https protocol

--- src/widgets/dropzone/assets/dropzone.init.js
+++ src/widgets/dropzone/assets/dropzone.init.js
@@ -1,7 +1,8 @@
 Dropzone.autoDiscover = false;
-
+const siteURL = document.location.protocol +'//'+ document.location.host;
+console.log(siteURL);
 var dropzone = new Dropzone("#dropzone", {
-    url: 'http://taskforce.local/work/add',
+    url: siteURL+'/work/add',
     addRemoveLinks: true,
     autoDiscover: false,
     acceptedFiles: 'image/*',
@@ -32,7 +33,7 @@ var dropzone = new Dropzone("#dropzone", {
     },
     removedfile: function (file) {
         $.ajax({
-            url: 'http://taskforce.local/work/remove?filename=' + file.name,
+            url: siteURL+'/work/remove?filename=' + file.name,
             type: 'GET',
             header: {
                 "_csrf-frontend": $(":input[name='_csrf-frontend']", $("form")).val()
@@ -54,14 +55,14 @@ var dropzone = new Dropzone("#dropzone", {
     init: function () {
         const thisDropzone = this;
         $.ajax({
-            url: 'http://taskforce.local/work/list',
+            url: siteURL+'/work/list',
             type: 'GET',
             success: function (data) {
                 $.each(JSON.parse(data), function (key, value) {
                     let mockFile = {name: value.name, size: value.size};
-                    thisDropzone.options.thumbnail.call(thisDropzone, mockFile, "http://taskforce.local/uploads/works/" + value.id + '/' + value.name);
+                    thisDropzone.options.thumbnail.call(thisDropzone, mockFile,siteURL+ "/uploads/works/" + value.id + '/' + value.name);
                     thisDropzone.emit("addedfile", mockFile);
-                    thisDropzone.emit("thumbnail", mockFile, "http://taskforce.local/uploads/works/" + value.id + '/' + value.name);
+                    thisDropzone.emit("thumbnail", mockFile, siteURL+"/uploads/works/" + value.id + '/' + value.name);
                     thisDropzone.emit("complete", mockFile);
                 })
             },
